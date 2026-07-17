11b6db524c92a173ecc4498fe5dbc35d7884c45e
Adam <test@test.com>
2026-07-18 00:06:24 +0300

    add health check endpiont

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ from fastapi import FastAPI
 
 import uvicorn
 
-
 app = FastAPI()
+
+app.get("")
+def health_check();
+        return{
+                "healthy":True
+        }
 
 if __name__ == "__main__":
         uvicorn.run("main:app",host="127.0.0.1", port=8000, reload=True)ַ
