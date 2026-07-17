2e0dc23bf1306830d0c893278ccf6df231885faf
Adam <test@test.com>
2026-07-17 23:29:48 +0300

    creat web server

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
-# this is first version 1.2
-print("Hello from my local computer!")
+# this is  version 1.2
+
+
+from fastapi import FastAPI
+
+import uvicorn
+
+
+app = FastAPI()
+
+if __name__ == "__main__":
+        uvicorn.run("main:app",host="127.0.0.1", port=8000, reload=True)ַ
